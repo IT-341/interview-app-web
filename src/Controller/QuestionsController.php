b2fa1b0a1f36d30417267a1f76b63972cc5454d3
Lucas Vieira <newlucasfevi@gmail.com>
2016-02-17 17:57:04 -0600

    Finished update action

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -14,7 +14,26 @@ class QuestionsController extends AppController
     {
         $response = $this->http->get('http://interview-app-server.herokuapp.com/api/question/' . $id);
 
-        // $this->set(['question' => $response->body('json_decode')]);
-        print($response->body); die;
+        $this->set(['question' => $response->body('json_decode')]);
+    }
+
+    public function update($id = null)
+    {
+        $response = $this->http->put(
+            'http://interview-app-server.herokuapp.com/api/question/' . $id,
+            [
+                'question' => $this->request->data['question'],
+                'answer'   => $this->request->data['answer']
+            ],
+            ['headers' => ['Content-Type' => 'x-www-form-urlencoded']]
+        );
+
+        if ($response->isOk()) {
+            $this->Flash->success('Question updated successfully!');
+        } else {
+            $this->Flash->error('Question update failed.');
+        }
+
+        return $this->redirect(['action' => 'show', $id]);
     }
 }
